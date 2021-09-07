98f5f79953c65fe1320c9b0ea0c1c9124bcb54d4
Anton Sizikov <tosha.sizikov@gmail.com>
2021-09-07 04:50:11 +0300

    wip

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -18,18 +18,12 @@ class MessageController extends Controller
     public function index($room = null)
     {
         if ($room) {
-            // return Message::where('hall_id', $room);
-            return Message::where('hall_id', $room)->whereDate('created_at', Carbon::today())->get();
-            // return Message::where('hall_id', $room)->where()
-            // return $hall->messages;
-            // return array_filter($hall->messages->toArray(), fn ($item) => date("d.m.y", strtotime($item["created_at"])) == date("d.m.y"));
-            // print_r($hall->messages->filter(fn ($item) => date("d.m.y", strtotime($item["created_at"])) == date("d.m.y"))->all());
-            // print_r($hall->messages);
-            // return $hall->messages->filter(fn ($item) => date("d.m.y", strtotime($item["created_at"])) == date("d.m.y"));
-
+            if ($room == 1 || $room == 2 || $room == 3) {
+                return Message::where('hall_id', $room)->whereDate('created_at', Carbon::today())->get();
+            } else {
+                return Message::where('hall_id', $room)->get();
+            }
         } else {
-            // return array_filter(Message::all()->toArray(), fn ($item) => date("d.m.y", strtotime($item["created_at"])) == date("d.m.y"));
-            // return array_filter(Message::all()->toArray(), fn ($item) => date("d.m.y", strtotime($item["created_at"])) == date("d.m.y"));
             return Message::all();
         }
     }
